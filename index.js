5f1e6707771b69818047837cc6bb83c709bc3ea7
jesusadenilza10-afk <jesusadenilza10@gmail.com>
2026-05-29 16:17:28 -0300

    finalizando

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-function mudar_conteudo_element(){
-
-  document.getElementById("h1").innerHTML = "Hello Javascript!";
-
-};
-
-function mudar_estilo(){
-
-document.getElementById("paragrafo").style.textAlign = "right";
+function mudar_paragrafo_1(){
+  document.getElementById("paragrafo1").style.fontSize = "1.6";
+  document.getElementById("paragrafo1").style.color = "green";
+  document.getElementById("paragrafo1").style.fontFamily =  "courier new";
+  document.getElementById("paragrafo1").style.textAlign = "left";
+  document.getElementById("paragrafo1").style.textTransform = "uppercase";
+}
+function mudar_paragrafo_2(){
+  document.getElementById("paragrafo2").style.fontSize = "1.3";
+  document.getElementById("paragrafo2").style.color = "blue";
+  document.getElementById("paragrafo2").style.fontFamily =  "Times new Roman";
+  document.getElementById("paragrafo2").style.textAlign = "right";
+  document.getElementById("paragrafo2").style.textTransform = "lowercase"
+}
+function mudar_paragrafo_3(){
+document.getElementById("paragrafo3").style.fontSize = "1.2";
+document.getElementById("paragrafo3").style.color = "orange";
+document.getElementById("paragrafo3").style.fontFamily =  "monospace";
+document.getElementById("paragrafo3").style.textAlign = "center";
+document.getElementById("paragrafo3").style.textTransform = "capitalize";
+}
+function mudar_(){
+  document.getElementById("paragrafo3").style.fontSize = "1.2";
+  document.getElementById("paragrafo3").style.color = "orange";
+  document.getElementById("paragrafo3").style.fontFamily =  "monospace";
+  document.getElementById("paragrafo3").style.textAlign = "center";
+  document.getElementById("paragrafo3").style.textTransform = "capitalize";
+}
+function mudar_regua_1(){
+  document.getElementById("regua3").style.border = "dotted green";
+}
+function mudar_regua_2(){
+  document.getElementById("regua3").style.border = "dashed pink";
+}
+function mudar_regua_3(){
+  document.getElementById("regua3").style.border = "solid blue";
 }
